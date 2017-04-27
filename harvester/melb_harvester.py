874569f5a11e6bdede4a14e538490aebd040a01d
shivisuper <shivank.shuman@gmail.com>
2017-04-27 20:58:16 +1000

    now harvester collects streamed tweets from entire australia. Exception handling also refined

--- harvester/melb_harvester.py
+++ harvester/melb_harvester.py
@@ -4,8 +4,8 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-from tweepy.utils import import_simplejson
-json = import_simplejson()
+# from tweepy.utils import import_simplejson
+# json = import_simplejson()
 ##################################################
 
 # tweepy error handling ##########################
@@ -29,21 +29,27 @@ consumer_secret = "PbU7wfXc68oSkzBNW1CQBnD1NVCrd7HlvKtpSPbxzGxsTj3mcK"
 ###########################################################################################################
 
 # Variables with city box grids ###########################################################################
-geobox_melbourne = [144.593742, -38.433859, 145.512529, -37.511274]
-geobox_sydney = [150.520929, -34.118347, 151.343021, -33.578141]
-geobox_brisbane = [152.668523, -27.767441, 153.317870, -26.996845]
-geobox_perth = [115.684048, -32.455642, 116.239023, -31.624485]
+# geobox_melbourne = [144.593742, -38.433859, 145.512529, -37.511274]
+# geobox_sydney = [150.520929, -34.118347, 151.343021, -33.578141]
+# geobox_brisbane = [152.668523, -27.767441, 153.317870, -26.996845]
+# geobox_perth = [115.684048, -32.455642, 116.239023, -31.624485]
+# geobox_final = geobox_melbourne + geobox_sydney + geobox_brisbane + geobox_perth
+geobox_australia = [110.951034, -54.833766, 159.287222, -9.187026]
 ############################################################################################################
 
 # Initializing objects for twilio rest client to send SMS notifications ####################################
-clientShivank = Client("ACd434aa13726a611ed78e241c9584a866", "9f532222976f5e6e38ef562aa68e5645")
-# clientRakesh = Client("AC29685c27da2195eaa5ff13851eec3f73", "2e0d2d0f537e1067aeb4a5be4ed7e3e0")
-clientJigar = Client("AC8316dbf0dfff0e545f80d7ef076106c9", "95db6786b4c7ce4f969d09f6f4f6cae6")
-geobox_final = geobox_melbourne + geobox_sydney + geobox_brisbane + geobox_perth
+client_shivank = Client("ACd434aa13726a611ed78e241c9584a866", "9f532222976f5e6e38ef562aa68e5645")
+shiv_to_num = "+61426274869"
+shiv_from_num = "+61439548489"
+client_jigar = Client("AC8316dbf0dfff0e545f80d7ef076106c9", "95db6786b4c7ce4f969d09f6f4f6cae6")
+jig_to_num = "+61406101797"
+jig_from_num = "+61409738448"
+
 ############################################################################################################
 
 # couchDB connection string and other constants ############################################################
-url = 'http://smoky:smoky@115.146.95.71:5984/rawtweetsdb'
+# master instance -> 115.146.95.71
+url = 'http://smoky:smoky@0.0.0.0:5984/rawtweetsdb'
 headers = {'Content-Type': 'application/json'}
 ############################################################################################################
 
@@ -51,85 +57,45 @@ headers = {'Content-Type': 'application/json'}
 # This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
     def on_data(self, json_data):
-        try:
-            tmp = json.loads(json_data)
-            # print(json_data)
-            requests.post(url=url, data=json_data, headers=headers)
-            return True
-        except ValueError as ve:
-            line_num = sys.exc_info()[2].tb_lineno
-            print(ve)
-            clientShivank.messages.create(to="+61426274869", from_="+61439548489",
-                                          body='At line {0}: {1}'.format(line_num, ve))
-            clientJigar.messages.create(to="+61406101797", from_="+61409738448",
-                                        body='At line {0}: {1}'.format(line_num, ve))
-            # don't stop the stream, just send the process tweet in queue instead
-            return True
-        except requests.exceptions.Timeout as timeout:
-            line_num = sys.exc_info()[2].tb_lineno
-            print(timeout)
-            clientShivank.messages.create(to="+61426274869", from_="+61439548489",
-                                          body='At line {0}: {1}'.format(line_num, timeout))
-            clientJigar.messages.create(to="+61406101797", from_="+61409738448",
-                                        body='At line {0}: {1}'.format(line_num, timeout))
-            return False
-        except requests.exceptions.ConnectionError as ce:
-            line_num = sys.exc_info()[2].tb_lineno
-            print(ce)
-            clientShivank.messages.create(to="+61426274869", from_="+61439548489",
-                                          body='At line {0}: {1}'.format(line_num, ce))
-            clientJigar.messages.create(to="+61406101797", from_="+61409738448",
-                                        body='At line {0}: {1}'.format(line_num, ce))
-            return False
-        except requests.exceptions.RequestException as re:
-            line_num = sys.exc_info()[2].tb_lineno
-            print(re)
-            clientShivank.messages.create(to="+61426274869", from_="+61439548489",
-                                          body='At line {0}: {1}'.format(line_num, re))
-            clientJigar.messages.create(to="+61406101797", from_="+61409738448",
-                                        body='At line {0}: {1}'.format(line_num, re))
-            return False
-        except Exception as e:
-            line_num = sys.exc_info()[2].tb_lineno
-            clientShivank.messages.create(to="+61426274869", from_="+61439548489",
-                                          body='At line {0}: {1}'.format(line_num, e))
-            clientJigar.messages.create(to="+61406101797", from_="+61409738448",
-                                        body='At line {0}: {1}'.format(line_num, e))
-            return False
+        # print(json_data)
+        requests.post(url=url, data=json_data, headers=headers)
+        return True
 
     def on_error(self, status):
-        line_num = sys.exc_info()[2].tb_lineno
         print(status)
-        clientShivank.messages.create(to="+61426274869", from_="+61439548489",
-                                      body='At line {0}: {1}'.format(line_num, status))
-        clientJigar.messages.create(to="+61406101797", from_="+61409738448",
-                                    body='At line {0}: {1}'.format(line_num, status))
+        notify_sms(msg=te.reason)
         return False
+
+
+# this function is used to notify devs via SMS whenever harvester stops
+def notify_sms(msg):
+    client_shivank.messages.create(to=shiv_to_num, from_=shiv_from_num,
+                                  body='Harvester stopped: {0}'.format(msg))
+    client_jigar.messages.create(to=jig_to_num, from_=jig_from_num,
+                                body='Harvester stopped: {0}'.format(msg))
 
 
 if __name__ == '__main__':
     # This handles Twitter authentication and the connection to Twitter Streaming API
-
-    # clientRakesh.messages.create(to="+61426848971", from_="+61745731256",
-    #                               body="Hello from python")
     listener = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, listener)
-    # This line filter Twitter Streams to capture data by the keywords:
     try:
-        stream.filter(locations=geobox_final)
+        # This line filter Twitter Streams to capture data by the keywords:
+        stream.filter(locations=geobox_australia)
     except TweepError as te:
-        line_num = sys.exc_info()[2].tb_lineno
-        print('At line {0}: {1}'.format(line_num, te.reason))
-        clientShivank.messages.create(to="+61426274869", from_="+61439548489",
-                                      body='At line {0}: {1}'.format(line_num, te.reason))
-        clientJigar.messages.create(to="+61406101797", from_="+61409738448",
-                                    body='At line {0}: {1}'.format(line_num, te.reason))
+        print(te.reason)
+        notify_sms(msg=te.reason)
+    except requests.exceptions.Timeout as timeout:
+        print(timeout)
+        notify_sms(msg=timeout)
+    except requests.exceptions.ConnectionError as ce:
+        print(ce)
+        notify_sms(msg=ce)
+    except requests.exceptions.RequestException as re:
+        print(re)
+        notify_sms(msg=re)
     except Exception as e:
-        line_num = sys.exc_info()[2].tb_lineno
         print('Exception: {0}'.format(e))
-        clientShivank.messages.create(to="+61426274869", from_="+61439548489",
-                                      body='At line {0}: {1}'.format(line_num, e))
-        clientJigar.messages.create(to="+61406101797", from_="+61409738448",
-                                    body='At line {0}: {1}'.format(line_num, e))
+        notify_sms(msg=e)
