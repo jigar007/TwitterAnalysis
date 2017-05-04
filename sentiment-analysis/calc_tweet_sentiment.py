6d70a02d06311389caa5c33f4fc5d52be3a5429a
Shivank <shivank.shuman@gmail.com>
2017-05-04 14:36:06 +1000

    commit before merge from sam for gitignore

--- sentiment-analysis/calc_tweet_sentiment.py
+++ sentiment-analysis/calc_tweet_sentiment.py
@@ -1,17 +1,24 @@
-import sys
 from textblob import TextBlob
 import re
 from emoji import EMOJIS
 import json
+from pymongo import MongoClient
+
+# mongodb connection string ##################
+mongo_client = MongoClient('115.146.95.71', 27017)
+raw_db = mongo_client['rawtweetsdbLive']
+collection = raw_db['test']
+##############################################
+
 
 def preprocesstweets(rawtext):
-
     # Replace hashtags with word
     tweet = rawtext.replace("#", "")
-
     # Remove URLs
     tweet = re.sub('((https?://[^\s]+)|(www\.[^\s]+))', '', tweet)
-    # To read the regex: first alternative is match http literally and then s? means match s where the ? quantifier matches between 0 and 1 times. Match :// literally and then the capturing group will match any single character isn't whitespace, where the + is a qunatifier meaning match between one and unlimited times
+    # To read the regex: first alternative is match http literally and then s? means match s where the ? quantifier
+    # matches between 0 and 1 times. Match :// literally and then the capturing group will match any single character
+    # isn't whitespace, where the + is a qunatifier meaning match between one and unlimited times
     # second alternative is match www. literally and then same capturing group as first alternative.
 
     # Remove any usernames (@username)
@@ -21,8 +28,8 @@ def preprocesstweets(rawtext):
     for emoji in EMOJIS:
         if emoji in tweet:
             tweet = tweet.replace(emoji, ' {} '.format(EMOJIS[emoji]))
-
     return tweet
+
 
 def calcSentiment(tweet):
     tweet = TextBlob(tweet)
@@ -36,18 +43,16 @@ def calcSentiment(tweet):
 
     return sentiment
 
+
 def main():
-    # print('this is the main')
-    # print('Number of arguments:', len(sys.argv), 'arguments.')
-    # print('Argument List:', str(sys.argv))
-
-    with open('tinyTwitter.json', encoding="utf8") as file:
-        data = json.load(file)
-
-    for raw_tweet in data:
-        proc_text = preprocesstweets(raw_tweet["json"]["text"])
-        sentiment = calcSentiment(proc_text)
-        print("RAWTEXT: {}\nPROCTEXT: {}\nSENTIMENT: {}\n".format(raw_tweet["json"]["text"],proc_text,sentiment))
+    for raw_tweet in collection.find():
+        # first pre-process the text in the tweet and find out it's sentiment
+        sentiment = calcSentiment(preprocesstweets(raw_tweet["text"]))
+        # create a new property for that document which stores the sentiment
+        collection.update({"_id": raw_tweet["_id"]},
+                          {"$set": {"tweet_sentiment": sentiment}}
+                          )
+        # print("RAWTEXT: {}\nPROCTEXT: {}\nSENTIMENT: {}\n".format(raw_tweet["text"],proc_text,sentiment))
 
 if __name__ == '__main__':
     main()
